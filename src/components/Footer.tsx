5140180703571f25bb1192ee99d1e09d62e5e941
Alejandro Salazar <asalazar@sticky.io>
2022-12-08 08:49:38 -0500

    Misc style changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,71 +14,6 @@ const useStyles = createStyles((theme) => ({
     }`,
   },
 
-  logo: {
-    maxWidth: 200,
-
-    [theme.fn.smallerThan("sm")]: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-    },
-  },
-
-  description: {
-    marginTop: 5,
-
-    [theme.fn.smallerThan("sm")]: {
-      marginTop: theme.spacing.xs,
-      textAlign: "center",
-    },
-  },
-
-  inner: {
-    display: "flex",
-    justifyContent: "space-between",
-
-    [theme.fn.smallerThan("sm")]: {
-      flexDirection: "column",
-      alignItems: "center",
-    },
-  },
-
-  groups: {
-    display: "flex",
-    flexWrap: "wrap",
-
-    [theme.fn.smallerThan("sm")]: {
-      display: "none",
-    },
-  },
-
-  wrapper: {
-    width: 160,
-  },
-
-  link: {
-    display: "block",
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[1]
-        : theme.colors.gray[6],
-    fontSize: theme.fontSizes.sm,
-    paddingTop: 3,
-    paddingBottom: 3,
-
-    "&:hover": {
-      textDecoration: "underline",
-    },
-  },
-
-  title: {
-    fontSize: theme.fontSizes.lg,
-    fontWeight: 700,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    marginBottom: theme.spacing.xs / 2,
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-  },
-
   afterFooter: {
     display: "flex",
     justifyContent: "space-between",
@@ -92,12 +27,6 @@ const useStyles = createStyles((theme) => ({
 
     [theme.fn.smallerThan("sm")]: {
       flexDirection: "column",
-    },
-  },
-
-  social: {
-    [theme.fn.smallerThan("sm")]: {
-      marginTop: theme.spacing.xs,
     },
   },
 }));
